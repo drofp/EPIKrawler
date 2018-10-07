f2e1cc3947c445a09a421e03e1aa7062bbddad1c
drp <rtotop@gmail.com>
2018-10-07 16:15:22 -0700

    Implement WASD movement

--- src/epikrawler.py
+++ src/epikrawler.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3.6
 
 import pygame
+from pygame.locals import *
 
 import sys
 import os
-sys.path.append(os.path.join(os.path.abspath(""),'frontend'))
+sys.path.append(os.path.join(os.path.abspath(""), 'frontend'))
 sys.path.append(os.path.join(os.path.abspath(""), 'backend'))
 
 from frontend import *
@@ -14,6 +15,7 @@ def main():
     clock = pygame.time.Clock()
 
     running = True
+    maxFPS = 60
 
     backgroundImg = colors.DARK_BLUE
 
@@ -29,7 +31,7 @@ def main():
         # Main event loop
         keysPressed = pygame.key.get_pressed()
         for event in pygame.event.get():
-            if event.type == pygame.QUIT or keysPressed[pygame.K_q]:
+            if event.type == pygame.QUIT or keysPressed[pygame.K_ESCAPE]:
                 running = False
                 pygame.display.quit()
                 sys.exit()
@@ -39,7 +41,11 @@ def main():
         mainScreen.fill(backgroundImg)
         mainPlayer.update_loc(mainScreen)
         pygame.display.update()
-        clock.tick(60) # in FPS
+        clock.tick(maxFPS)
+
+        # For rough benchmarking runtime quickly
+        # TODO: Show on window, like here: https://www.youtube.com/watch?v=HBbzYKMfx5Y
+        print(clock.get_fps())
 
 if __name__ == '__main__':
     main()
